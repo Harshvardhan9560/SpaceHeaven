d3adc23f761f340db9bcaa3b4082bcbbdab6fa82
Harshvardhan Singh <harshvardhan.singh.work09@gmail.com>
2026-08-04 13:48:26 +0530

    Remove node_modules, add .gitignore, connect Atlas

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Listing = require("./models/listing");
-const methodOverride = require("method-override"); 
-const { equal } = require("assert");
-const MONGO_URL = "mongodb://127.0.0.1:27017/SpaceHeaven";
+const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
-
+const MONGO_URL = process.env.ATLASDB_URL;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
