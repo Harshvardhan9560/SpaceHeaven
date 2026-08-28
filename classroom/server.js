1f7dcafc29d69f0719d47e76b4889c4b829dab5b
Harshvardhan Singh <harshvardhan.singh.work09@gmail.com>
2026-08-28 16:15:30 +0530

    cookie-parse

--- classroom/server.js
+++ classroom/server.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
+const cookiesParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 app.get("/getcookies", (req,res)=>{
     res.cookie("greet","hello");
     res.send("sent your cookies! ");
 })
 
+app.get("/greet" , (req,res)=>{
+    let {name= "anonymous"} = req.cookies;
+    res.send(`Hi,${name} root!`);
+})
+
 
 
 
 app.get("/", (req, res) => {
+    console.dir(req.cookies);
+    
   res.send("Hi, I am root!");
 
 });
