ec63ea8fabe96e441b0b4575d16bf9bb176b0bfa
Harshvardhan Singh <harshvardhan.singh.work09@gmail.com>
2026-08-28 15:50:43 +0530

    updated user routes

---
+++ classroom/routes/user.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Index 
+
+router.get("/" ,(req,res)=>{
+
+    res.send("Get for users");
+
+} );
+
+//show -users
+
+router.get("/:id" , (req,res)=>{
+
+    res.send("get for users");
+
+});
+
+// post-users
+
+router.post("/", (req,res)=>{
+
+    res.send("post for users");
+
+})
+
+// delete-users
+
+router.post("/:id", (req,res)=>{
+
+    res.send("Delete for users id");
+
+})
+
+
+module.exports = router;
