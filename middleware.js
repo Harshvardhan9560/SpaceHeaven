929bf6c58f2ff36c43e507ab0236a265e78059e9
Harshvardhan Singh <harshvardhan.singh.work09@gmail.com>
2026-09-01 20:40:20 +0530

    post-login

--- middleware.js
+++ middleware.js
@@ -1,4 +1,7 @@
 module.exports.isLoggedIn = (req,res,next)=>{
+   // redirecturl save 
+   req.session.redirectUrl = req.originalUrl;
+    
 if (!req.isAuthenticated()) {
         req.flash("success", "Login in to Create listing!");
         return res.redirect("/login");
@@ -6,3 +9,9 @@ if (!req.isAuthenticated()) {
     next();
 }
 
+module. exports. saveRedirectUrl = (req, res, next) =>{
+if (req.session. redirectUrl) {
+res. locals. redirectUrl = req.session.redirectUrl;
+}
+next();
+};
