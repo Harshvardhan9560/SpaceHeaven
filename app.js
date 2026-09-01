7239a031e9e857834be3effaa2273b847ce40732
Harshvardhan Singh <harshvardhan.singh.work09@gmail.com>
2026-09-01 20:07:29 +0530

    login-update

--- app.js
+++ app.js
@@ -63,6 +63,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use((req, res, next) => {
+    res.locals.currUser = req.user;
+    next();
+});
+
 // ---------------- LOCALS ----------------
 
 app.use((req, res, next) => {
--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,13 @@ router.post("/signup", wrapAsync(async(req,res)=>{
       let {username,email,password} =req.body;
     const newUser = new User({email,username});
    const registeredUser  = await User.register(newUser,password);
+   req.login
    console.log(registeredUser);
+   req.login(registeredUser,(err)=>{
+    if(err){
+        return next(err);
+    }
+   })
    req.flash("success", "Account created successfully!");
    res.redirect("/listings");
   }
