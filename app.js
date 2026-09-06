c3209305e17b963565eade2a78e292e010031322
Harshvardhan Singh <harshvardhan.singh.work09@gmail.com>
2026-09-06 10:01:04 +0530

    end

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-if(process.env.NODE_ENV != "production"){
-require("dotenv").config();
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
 }
-
-
 
 const express = require("express");
 const app = express();
@@ -11,6 +9,9 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+
+const connectMongo = require("connect-mongo");
+const MongoStore = connectMongo.default;
 
 const session = require("express-session");
 const flash = require("connect-flash");
@@ -21,14 +22,26 @@ const User = require("./models/user.js");
 const listingsRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
-const { reviewSchema } = require("./schema.js");
 
 const MONGO_URL = process.env.ATLASDB_URL;
 
 // ---------------- SESSION ----------------
 
+const store = MongoStore.create({
+    mongoUrl: MONGO_URL,
+    crypto: {
+        secret: process.env.SESSION_SECRET || "mysupersecretstring"
+    },
+    touchAfter: 24 * 3600
+});
+
+store.on("error", (err) => {
+    console.log("Error in Mongo Session Store", err);
+});
+
 const sessionOptions = {
-    secret: "mysupersecretstring",
+    store,
+    secret: process.env.SESSION_SECRET || "mysupersecretstring",
     resave: false,
     saveUninitialized: true,
 
@@ -67,18 +80,12 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req, res, next) => {
-    res.locals.currUser = req.user;
-    next();
-});
-
-
 // ---------------- LOCALS ----------------
 
 app.use((req, res, next) => {
+    res.locals.currUser = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
 
     next();
 });
@@ -101,35 +108,16 @@ main()
 
 app.get("/", (req, res) => {
     req.flash("error", "Page Not Found!");
-   
+    res.redirect("/listings");
 });
+
 // ---------------- ROUTES ----------------
 
 app.use("/listings", listingsRouter);
 
 app.use("/", userRouter);
 
-// ---------------- REVIEW VALIDATION ----------------
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-
-    if (error) {
-        const errMsg = error.details
-            .map((el) => el.message)
-            .join(",");
-
-        throw new ExpressError(400, errMsg);
-    }
-
-    next();
-};
-
-app.use(
-    "/listings/:id/reviews",
-   
-    reviewsRouter
-);
+app.use("/listings/:id/reviews", reviewsRouter);
 
 // ---------------- 404 ----------------
 
@@ -143,8 +131,15 @@ app.use((err, req, res, next) => {
     console.log(err);
     console.log(err.stack);
 
-    let { statusCode = 500, message = "Something went wrong" } = err;
-    res.status(statusCode).send({ statusCode, message });
+    let {
+        statusCode = 500,
+        message = "Something went wrong"
+    } = err;
+
+    res.status(statusCode).send({
+        statusCode,
+        message
+    });
 });
 
 // ---------------- SERVER ----------------
